36078420aa379533c75ece2ce4db47ac00ac7271
K.metehancaliskan <metehan.caliskan@paratica.com>
2022-09-17 16:24:55 +0300

    http server

--- protocols/decryption.py
+++
@@ -1,5 +0,0 @@
-import jwt
-import encryption as enc
-key=enc.key
-encoded = enc.encoded
-jwt.decode(encoded, "secret", algorithms=["HS256"])
--- protocols/encryption.py
+++
@@ -1,4 +0,0 @@
-import jwt
-from cryptography.fernet import Fernet
-key = Fernet.generate_key()
-encoded = jwt.encode({"some": "payload"}, key, algorithm="HS256")
